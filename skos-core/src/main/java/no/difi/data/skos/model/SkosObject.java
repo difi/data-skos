8d97de89e361b759ca6c4f804595646a5d818398
Erlend Klakegg Bergheim <erlend@klakegg.net>
2016-02-03 08:33:06 +0100

    Fixing notation.

--- skos-core/src/main/java/no/difi/data/skos/model/SkosObject.java
+++ skos-core/src/main/java/no/difi/data/skos/model/SkosObject.java
@@ -38,7 +38,7 @@ public abstract class SkosObject {
     }
 
     public List<String> getNotation() {
-        return notation;
+        return notation == null ? new ArrayList<String>() : notation;
     }
 
     @SuppressWarnings("unused")
